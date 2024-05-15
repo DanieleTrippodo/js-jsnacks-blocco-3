2ee1a331de04c52891ff44d4ddb1f4810fe0110a
DanieleTrippodo <trippododaniele@gmail.com>
2024-05-15 15:00:33 +0200

    Update Array-lista

--- array-lista/script.js
+++ array-lista/script.js
@@ -9,11 +9,36 @@ const Tavolo = 'pesca';
 Frigo.push(Tavolo);
 
 
+// impostiamo il valore trovato di default a false
+let trovato = false;
 
 
-/* verifichiamo se dentro al frigo abbiamo "cocomero"  */
-if(Frigo.includes('cocomero')){
+// controlliamo se in ogni elemento della Array Frigo, è presente il cocomero con un ciclo FOR
+for (let i=0; i < Frigo.length; i++){
+
+    // se trova il cocomero allora imposta il valore Trovata su True
+    if(Frigo[i] === 'cocomero'){
+        trovata = true;
+    }
+}
+
+
+
+if(trovata = true){
     console.log('Trovato! Devo solo preparare il cocktail')
-} else{
+} else {
     console.log('Oh no, devo uscire a comprare il cocomero!')
 }
+
+
+// // # Metodo più facile, ma che non possiamo usare!
+// /* verifichiamo se dentro al frigo abbiamo "cocomero"  */
+// if(Frigo.includes('cocomero')){
+//     console.log('Trovato! Devo solo preparare il cocktail')
+// } else{
+//     console.log('Oh no, devo uscire a comprare il cocomero!')
+// }
+// // # ---------------------------------------------
+
+
+
