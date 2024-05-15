9422b39b8c10d431e7884bd6f087ca2f0f275498
DDD <163750523+DanieleTrippodo@users.noreply.github.com>
2024-05-15 12:29:21 +0200

    Updated Snack 10

--- Snack 10/script.js
+++ Snack 10/script.js
@@ -1,23 +1,23 @@
-// creiamo i nostri 2 array vuoti
-let Array1 = [1,2,3];
-let Array2 = [4,5,6,7,8,9];
+// Creiamo i nostri due array con un numero diverso di elementi
+let Array1 = [1, 2, 3];
+let Array2 = [4, 5, 6, 7, 8, 9];
 console.log('Valore iniziale:', Array1);
 
 
 
 
-// controlliamo quale dei due array sia il più corto
-while (Array1.length > Array2.length){    // nel caso in cui Array1 fosse maggiore di Array2
 
-    Array2.push(Math.floor( Math.random() * 10));
+// Controlliamo quale dei due array sia il più corto e aggiungiamo elementi all'array più corto finché non ha lo stesso numero di elementi dell'altro array
+while (Array1.length < Array2.length) { // se Array1 ha meno elementi di Array2
+    Array1.push(Math.floor(Math.random() * 10)); // aggiungiamo un numero casuale ad Array1
+}
+
+while (Array2.length < Array1.length) { // se Array2 ha meno elementi di Array1
+    Array2.push(Math.floor(Math.random() * 10)); // aggiungiamo un numero casuale ad Array2
 }
 
 
 
-// controlliamo quale dei due array sia il più corto
-while (Array2.length > Array1.length){    // nel caso in cui Array2 fosse maggiore di Array1
 
-    Array1.push(Math.floor( Math.random() * 10));
-}
-
-console.log(Array1);
+console.log('Array1:', Array1);
+console.log('Array2:', Array2);
